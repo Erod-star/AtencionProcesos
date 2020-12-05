17bc7f43d875c89e1a3d6e550437d8b1fd82bc99
Erod-star <erodriguez43@ucol.mx>
2020-12-04 22:59:44 -0600

    Añadir pre-final con correción de listas circulares

--- procesador.js
+++ procesador.js
@@ -1,30 +1,90 @@
-class Procesador {
-    constructor(){
-        this.ciclos=ciclosAsimular;
-        this.patendidos
-        this.ciclosvacios
-        this.memoria = new ListaCircular()
+const Proceso = require('./tarea').default;
+
+let Procesador = () => {
+    let inicio = null;
+    let tamaño = 0;
+    let terminado = 0;
+    let vacio = 0;
+    let ultimo = null;
+
+    const creacionTarea = (tarea) => {
+        if (inicio == null) {
+            inicio = tarea;
+            ultimo = tarea;
+            inicio.siguiente = tarea;
+        } else {
+            tarea.siguiente = inicio;
+            ultimo.siguiente = tarea;
+            ultimo = tarea;
+        }
     }
 
-    simular(ciclosAsimular){
-        let memoria = new ListaCircular()
-        for(let i=0;i<ciclosAsimular;i++){
-            numero = Math.ceil(Math.random() * 100);
-
-            if (numero<=39){ //nuevo proceso
-                let nuevo=new Proceso()
-                memoria.agregar(nuevo)
-            } 
-
-            if (memoria.inicio != null){ //
-                memoria.inicio.ciclos--;
+    const iniciar = () => {
+        let numeroTarea = 1;
+        for (let i = 0; i < 300; i++) {
+            let x = Math.floor(Math.random() * (100-1)+1);
+            if (inicio == null) {
+                vacio ++;
             }
-            if (memoria.inicio.ciclos==0){
-                this.patendidos++;
-                memoria.eliminarInicio(); //elimina y en automatico va al sig
-            }else{
-                 memoria.avanza()
+            if (x <= 39) {
+                let tarea = new Tarea(numeroTarea);
+                creacionTarea(tarea);
+                numeroTarea++;
+                tamaño++;
             }
-        }   
+            var aux = inicio;
+            if (aux != null) {
+                if (aux.numCiclos == 0) {
+                    eliminar(aux);
+                    tamaño--;
+                    terminado++;
+                }
+                aux.numCiclos--;
+                aux = aux.siguiente;
+            }
+            imprimir();
+        }
     }
+
+    const search = (aux) => {
+        let actual = inicio;
+        if (actual == aux) {
+            return ultimo;
+        } else {
+            while (actual.siguiente != aux && ultimo != aux) {
+                actual = inicio.siguiente;
+            }
+            return actual;
+        }
+    }
+
+    const eliminar = (aux) => {
+        let anterior = search(aux);
+        if (aux == inicio && aux == ultimo) {
+            inicio = null;
+            ultimo = null;
+        } else if (aux == inicio){
+            inicio = aux.siguiente;
+            ultimo.siguiente = inicio;
+        } else if (aux == ultimo){
+            anterior.siguiente = inicio;
+            ultimo = anterior;
+        } else
+            anterior.siguiente = aux.siguiente;
+    }
+
+    const imprimir = () => {
+        console.log(
+            `
+            Ciclos vacíos ${vacio},
+            Proceso terminado ${terminado},
+            Procesos pendientes ${tamaño}
+            `
+        );
+    }
+    return {iniciar:iniciar, imprimir:imprimir}
 }
+
+
+
+exports.default = Procesador;
